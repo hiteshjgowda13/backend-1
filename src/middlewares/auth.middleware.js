ee88f1a0c3ef9cadc7a2468f93c73f0edb7609a7
Hitesh <hiteshjgowda13@gmail.com>
2026-05-12 21:41:33 +0530

    middleware for auth of user

---
+++ src/middlewares/auth.middleware.js
@@ -0,0 +1,32 @@
+import { User } from "../models/users.model.js";
+import { apiError } from "../utils/apiError.js";
+import { asyncHandler } from "../utils/asyncHandler.js";
+import jwt from "jsonwebtoken"
+
+// if we want to omit res we replace it with underscore (_)
+export const verifyJWT = asyncHandler(async (req, _,next) =>{
+try {
+    //get access token here 
+    const token = req.cookies?.accessToken || req.header("Authorization")?.replace("Bearer ","")
+    
+    //check if token is there else give error negative check
+    if(!token){
+        throw new apiError(401,"unauthorized request")
+    }
+    //verify the token with our token and return decoded value using jwt.verify
+    const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET) //returns decoded value
+        
+    // get user from db and remove password and refresh token from him after
+    const user = await User.findById(decodedToken?._id).select("-password -refreshToken")
+    
+    
+    if(!user){
+        throw new apiError(401,"Invalid access Token")
+    }
+    
+    req.user = user;
+    next()
+} catch (error) {
+    throw new apiError(401, error?.message || "Invalid access Token")
+}
+})
